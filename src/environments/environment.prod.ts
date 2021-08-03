0939c5c9ce72697abc58baccc8c8ed16d539e9a1
jgu7man <jgu7man@gmail.com>
2021-08-02 19:16:24 -0500

    set to github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
+  restURL: 'https://us-central1-main-agentesmart.cloudfunctions.net/dialogflow/',
+  apiURL: 'https://api.agentesmart.com',
+  websocketURL: 'ws://agentesmart-ws.herokuapp.com/',
   firebaseConfig: {
 
   }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  restURL: 'http://localhost:5001/main-agentesmart/us-central1/rest',
+  apiURL: 'http://localhost:5000',
+  websocketURL: 'ws://localhost:8999',
+  firebaseConfig: {
+
+  }
+};
